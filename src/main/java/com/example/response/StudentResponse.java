fde669c11c9a2368bd392034d193d551eac38b8b
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-28 07:02:19 -0400

    Setting up java response, service, and main class to connect to db

--- src/main/java/com/example/response/StudentResponse.java
+++ src/main/java/com/example/response/StudentResponse.java
@@ -1,23 +1,47 @@
 package com.example.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.ArrayList;
+import java.util.List;
 
-import lombok.AllArgsConstructor;
+import com.example.entity.Student;
+import com.example.entity.Subject;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class StudentResponse {
 
-	@JsonIgnore
 	private long id;
 
-	@JsonProperty("first_name")
 	private String firstName;
 
 	private String lastName;
+
+	private String email;
+	
+	private String street;
+
+	private String city;
+	
+	private List<SubjectResponse> learningSubjects;
+	
+	public StudentResponse (Student student) {
+		this.id = student.getId();
+		this.firstName = student.getFirstName();
+		this.lastName = student.getLastName();
+		this.email = student.getEmail();
+		
+		this.street = student.getAddress().getStreet();
+		this.city = student.getAddress().getCity();
+		
+		if (student.getLearningSubjects() != null) {
+			learningSubjects = new ArrayList<SubjectResponse>();
+			for (Subject subject: student.getLearningSubjects()) {
+				learningSubjects.add(new SubjectResponse(subject));
+			}
+		}
+	}
 
 }
