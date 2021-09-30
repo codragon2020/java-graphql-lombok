9a0fbdcb4de5d732fe6bab9a668a71aa9f4c60d4
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-09-30 05:48:34 -0400

    Data filter for GraphQL with subject name

--- src/main/java/com/example/resolver/StudentResponseResolver.java
+++ src/main/java/com/example/resolver/StudentResponseResolver.java
@@ -11,13 +11,16 @@ import com.example.response.SubjectResponse;
 @Service
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
 
-	public List<SubjectResponse> getLearningSubjects (StudentResponse studentResponse) {
+	public List<SubjectResponse> getLearningSubjects (StudentResponse studentResponse, SubjectNameFilter subjectNameFilter) {
 		
     	List<SubjectResponse> learningSubjects = new ArrayList<SubjectResponse>();
 
         if (studentResponse.getStudent().getLearningSubjects() != null) {
 			for (Subject subject: studentResponse.getStudent().getLearningSubjects()) {
-				learningSubjects.add(new SubjectResponse(subject));
+                if (subjectNameFilter.name().equalsIgnoreCase(subject.getSubjectName(0))) {
+    				learningSubjects.add(new SubjectResponse(subject));
+
+                }
 			}
 		}
 
